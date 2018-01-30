37d59c7c41c2c3fa7e369c46a1c72ec8c0530b77
alvanedgar <alvan00000@gmail.com>
2018-01-30 18:22:15 -0500

    First Commit

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMove : MonoBehaviour
+{
+    public Transform[] waypoints;
+    int cur = 0;
+
+    public float speed = 0.5f;
+
+    void FixedUpdate()
+    {
+        // If waypoint is not reached, move closer
+        if (transform.position != waypoints[cur].position)
+        {
+            Vector2 p = Vector2.MoveTowards(transform.position, waypoints[cur].position, speed);
+            GetComponent<Rigidbody2D>().MovePosition(p);
+        }
+        // If waypoint is reached, go to the next one
+        else cur = (cur + 1) % waypoints.Length;
+
+        // Animation
+        Vector2 dir = waypoints[cur].position - transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if(co.name == "pacman")
+        {
+            Destroy(co.gameObject);
+        }
+    }
+}
